6292cb1da872959db0198333f1ee02a0f10215fd
KawaiiTenshi555 <83819316+KawaiiTenshi555@users.noreply.github.com>
2026-05-13 23:16:43 +0200

    Rollback automatic guild discovery

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,6 @@
-const { getGuildConfig, listConfiguredGuilds, saveGuildConfig } = require('../utils/config');
-const { registerGuildCommands } = require('../utils/commands');
+const { REST, Routes } = require('discord.js');
+const { getCommands } = require('../commands/index');
+const { listConfiguredGuilds } = require('../utils/config');
 
 module.exports = {
   name: 'ready',
@@ -7,17 +8,8 @@ module.exports = {
   async execute(client) {
     console.log(`[Bot] Connecte en tant que ${client.user.tag}`);
 
-    for (const [, guild] of client.guilds.cache) {
-      if (!getGuildConfig(guild.id, { requireEnabled: false })) {
-        saveGuildConfig(guild.id, {
-          guildId: guild.id,
-          name: guild.name,
-          enabled: true,
-        });
-        console.log(`[Bot] Serveur ajoute a la configuration: ${guild.name} (${guild.id}).`);
-      }
-    }
-
+    const commands = getCommands();
+    const rest = new REST().setToken(process.env.BOT_TOKEN);
     const guilds = listConfiguredGuilds({ enabledOnly: true });
 
     if (!guilds.length) {
@@ -27,7 +19,10 @@ module.exports = {
 
     for (const guild of guilds) {
       try {
-        await registerGuildCommands(guild.guildId);
+        await rest.put(
+          Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.guildId),
+          { body: commands.map(c => c.data.toJSON()) },
+        );
         console.log(`[Bot] Slash commands enregistrees pour ${guild.name || guild.guildId}.`);
       } catch (err) {
         console.error(`[Bot] Erreur enregistrement commands pour ${guild.guildId}:`, err);
